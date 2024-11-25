fc685fde7ab0688b3dce235afb85c2bf3ad15a1a
Gustavo Silva <Gustavo_Silva@McKinsey.com>
2024-11-25 08:43:40 -0300

    Initial Commit

---
+++ src/main/java/com/demo/spring_json_logging_demo/SpringJsonLoggingDemoApplication.java
@@ -0,0 +1,13 @@
+package com.demo.spring_json_logging_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJsonLoggingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJsonLoggingDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/spring_json_logging_demo/SpringJsonLoggingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.spring_json_logging_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJsonLoggingDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
